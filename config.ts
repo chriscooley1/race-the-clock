a4a9d8890bcf76c8b49cdf1939400e67e70430a9
Chris Cooley <ccooley@bonadmin.com>
2024-08-09 08:35:41 -0600

    Modify config

--- config.ts
+++ config.ts
@@ -1,3 +1,12 @@
-export const config = {
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000",
+const localConfig = {
+  API_BASE_URL: "http://localhost:8000",
+  ALLOWED_ORIGINS: ["http://localhost:5173"],
 };
+
+const productionConfig = {
+  API_BASE_URL: "https://your-production-api.com",
+  ALLOWED_ORIGINS: ["https://chriscooley1.github.io/race-the-clock/"],
+};
+
+export const config =
+  process.env.NODE_ENV === "production" ? productionConfig : localConfig;
