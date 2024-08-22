ac30c3be12508522cfe75cedf1b8b64860a70a71
Chris Cooley <ccooley@bonadmin.com>
2024-08-21 23:22:50 -0600

    Change styling for Preview

--- frontend/src/components/CollectionPreviewModal/CollectionPreviewModal.tsx
+++ frontend/src/components/CollectionPreviewModal/CollectionPreviewModal.tsx
@@ -5,7 +5,6 @@ import "../../App.css"; // Global styles for the app
 interface ModalProps {
   collection: {
     name: string;
-    description: string;
     items: { name: string }[];
   };
   onClose: () => void;
@@ -13,20 +12,25 @@ interface ModalProps {
 
 const CollectionPreviewModal: React.FC<ModalProps> = ({ collection, onClose }) => {
   return (
-    <div className="modal-background">
+    <div className="modal-background" onClick={(e) => {
+        if ((e.target as HTMLElement).className === "modal-background") {
+          onClose();
+        }
+      }}>
       <div className="modal-container">
         <h1>{collection.name}</h1>
-        <p>{collection.description}</p>
-        {collection.items.length > 0 ? (
-          <ul>
-            {collection.items.map((item, index) => (
-              <li key={index}>{item.name}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No items available in this collection.</p>
-        )}
-        <button type="button" onClick={onClose}>Close Preview</button>
+        <div className="list-item">
+          {collection.items.length > 0 ? (
+            collection.items.map((item, index) => (
+              <div key={index} className="list-item-container">
+                <p>{index + 1} - {item.name}</p>
+              </div>
+            ))
+          ) : (
+            <p>No items available in this collection.</p>
+          )}
+        </div>
+        <button type="button" className="close-button" onClick={onClose}>Close Preview</button>
       </div>
     </div>
   );
