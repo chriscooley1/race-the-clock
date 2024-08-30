59c1afa36e7580e80b04184620227cb49d895e28
Chris Cooley <ccooley@bonadmin.com>
2024-08-30 11:56:20 -0600

    Add port 3000

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), ""); // Loads environment variables directly
@@ -17,6 +18,16 @@ export default defineConfig(({ mode }) => {
           chunkFileNames: `assets/[name].[hash].js`,
           assetFileNames: `assets/[name].[hash].[ext]`,
         },
+      },
+    },
+    server: {
+      host: true, // This allows the server to be accessible over the network
+      port: 3000, // Adjust the port as necessary
+      historyApiFallback: true, // Ensures that the index.html is served for all routes
+    },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
       },
     },
   };
