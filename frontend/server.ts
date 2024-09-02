cf83b944893ccae001a5503a50d833d0c253e8a8
Chris Cooley <ccooley@bonadmin.com>
2024-09-02 00:42:38 -0600

    New Cors fix

--- frontend/server.ts
+++ frontend/server.ts
@@ -1,8 +1,26 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 import { Request, Response } from "express";
 
 const app = express();
+
+// Define allowed origins
+const allowedOrigins = ['https://race-the-clock-frontend-production.up.railway.app'];
+
+app.use(cors({
+    origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+        // Allow requests with no origin, like mobile apps or curl requests
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true); // Origin is allowed
+        } else {
+            callback(new Error('Not allowed by CORS')); // Origin is not allowed
+        }
+    },
+    credentials: true // Allow cookies to be sent along with requests
+}));
 
 app.use(express.static(path.join(__dirname, "dist")));
 
