80f05c213ad72c9ea36f5be29dbe9a30847a63f1
Chris Cooley <ccooley@bonadmin.com>
2024-09-01 23:47:05 -0600

    Fix mode error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv, ConfigEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode }: ConfigEnv) => {
   const env = loadEnv(mode, process.cwd(), ""); // Loads environment variables directly
 
   return {
