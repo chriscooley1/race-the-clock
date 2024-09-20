007cb96c8322860a2674c74d39643d1132729468
Chris Cooley <ccooley@bonadmin.com>
2024-09-20 17:45:22 -0600

    Test new approach for fixing CORS

--- backend/main.py
+++ backend/main.py
@@ -69,8 +69,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
 )
 
 # Auth0 token validation
