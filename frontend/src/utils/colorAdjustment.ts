c7564f72833f23ce2cee1e54318993ceb550886c
Chris Cooley <ccooley@bonadmin.com>
2024-10-16 11:47:17 -0600

    Change colorblind modes using lowercase

--- frontend/src/utils/colorAdjustment.ts
+++ frontend/src/utils/colorAdjustment.ts
@@ -20,59 +20,62 @@ export function adjustColorForColorblindness(
   type: string,
 ): string {
   console.log("Adjusting color:", color, "for type:", type); // Debugging line
+  if (!type || type.toLowerCase() === "none") {
+    return color;
+  }
+
   const rgb = hexToRgb(color);
   let adjusted: RGB;
 
-  switch (type) {
-    case "Protanopia":
+  switch (type.toLowerCase()) {
+    case "protanopia":
       adjusted = [
         0.567 * rgb[0] + 0.433 * rgb[1],
         0.558 * rgb[0] + 0.442 * rgb[1],
         0.242 * rgb[0] + 0.758 * rgb[2],
       ];
       break;
-    case "Protanomaly":
+    case "protanomaly":
       adjusted = [
         0.817 * rgb[0] + 0.183 * rgb[1],
         0.333 * rgb[0] + 0.667 * rgb[1],
         0.125 * rgb[0] + 0.875 * rgb[2],
       ];
       break;
-    case "Deuteranopia":
+    case "deuteranopia":
       adjusted = [
         0.625 * rgb[0] + 0.375 * rgb[1],
         0.7 * rgb[0] + 0.3 * rgb[1],
         0.3 * rgb[0] + 0.7 * rgb[2],
       ];
       break;
-    case "Deuteranomaly":
+    case "deuteranomaly":
       adjusted = [
         0.8 * rgb[0] + 0.2 * rgb[1],
         0.258 * rgb[0] + 0.742 * rgb[1],
         0.142 * rgb[0] + 0.858 * rgb[2],
       ];
       break;
-    case "Tritanopia":
+    case "tritanopia":
       adjusted = [
         0.95 * rgb[0] + 0.05 * rgb[2],
         0.433 * rgb[0] + 0.567 * rgb[1],
         0.475 * rgb[0] + 0.525 * rgb[2],
       ];
       break;
-    case "Tritanomaly":
+    case "tritanomaly":
       adjusted = [
         0.967 * rgb[0] + 0.033 * rgb[2],
         0.733 * rgb[0] + 0.267 * rgb[1],
         0.183 * rgb[0] + 0.817 * rgb[2],
       ];
       break;
-    case "Achromatopsia": {
+    case "achromatopsia": {
       const grayscale = 0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2];
       adjusted = [grayscale, grayscale, grayscale];
       break;
     }
-    case "Anomalous Trichromacy": {
-      // This is a general case and might need further refinement
+    case "anomalous trichromacy": {
       adjusted = [
         0.8 * rgb[0] + 0.2 * rgb[1],
         0.2 * rgb[0] + 0.8 * rgb[1],
@@ -87,9 +90,11 @@ export function adjustColorForColorblindness(
   console.log("Original RGB:", rgb);
   console.log("Adjusted RGB:", adjusted);
 
-  return rgbToHex(
+  const result = rgbToHex(
     Math.round(adjusted[0]),
     Math.round(adjusted[1]),
     Math.round(adjusted[2]),
   );
+  console.log("Adjusted color:", result);
+  return result;
 }
