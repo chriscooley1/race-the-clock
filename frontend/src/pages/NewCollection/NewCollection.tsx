c6d3794ed5556cc410cf664ce9f0ca6dbc0690bf
Chris Cooley <ccooley@bonadmin.com>
2024-12-04 09:00:01 -0700

    Add Layout to make CollectionsNavBar to be on all pages

--- frontend/src/pages/NewCollection/NewCollection.tsx
+++ frontend/src/pages/NewCollection/NewCollection.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useTheme } from "../../context/ThemeContext";
 import { tourStepsNewCollection } from "./tourStepsNewCollection";
 import GuidedTour from "../../components/GuidedTour";
+import Layout from "../../components/Layout";
 import FeedbackForm from "../../components/FeedbackForm";
 import { VisibilityStates } from "../../types/VisibilityStates";
 
@@ -124,97 +125,103 @@ const NewCollection: React.FC = () => {
   const [showFeedback, setShowFeedback] = useState<boolean>(false);
 
   return (
-    <div className="page-container mt-4">
-      <div className="mx-auto flex max-w-[600px] flex-col items-center">
-        <h1 className="new-collection-page text-4xl font-bold">
-          Step 1 - Create
-        </h1>
-        <h1 className="text-3xl font-bold">New Collection</h1>
+    <Layout
+      onStartTour={() => setIsTourRunning(true)}
+      setTourName={() => {}}
+      setCurrentTourStep={setCurrentTourStep}
+    >
+      <div className="page-container mt-4">
+        <div className="mx-auto flex max-w-[600px] flex-col items-center">
+          <h1 className="new-collection-page text-4xl font-bold">
+            Step 1 - Create
+          </h1>
+          <h1 className="text-3xl font-bold">New Collection</h1>
 
-        {visibilityStates.isCollectionNameVisible && (
-          <div className="mb-4 max-w-[300px] border border-black">
-            <input
-              type="text"
-              id="collectionName"
-              className={`collection-name-input font-teacher rounded border border-[var(--text-color)] p-2 text-center text-base ${theme.isDarkMode ? "bg-gray-700 text-white" : "bg-white text-black"}`}
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Collection Name"
-              title="Collection Name"
-              onKeyDown={handleKeyDown}
-            />
-          </div>
-        )}
+          {visibilityStates.isCollectionNameVisible && (
+            <div className="mb-4 max-w-[300px] border border-black">
+              <input
+                type="text"
+                id="collectionName"
+                className={`collection-name-input font-teacher rounded border border-[var(--text-color)] p-2 text-center text-base ${theme.isDarkMode ? "bg-gray-700 text-white" : "bg-white text-black"}`}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Collection Name"
+                title="Collection Name"
+                onKeyDown={handleKeyDown}
+              />
+            </div>
+          )}
 
-        {visibilityStates.isCategorySelectVisible && (
-          <div className="mb-4 flex max-w-[300px] items-center justify-center">
-            <label htmlFor="categorySelect" className="mr-2">
-              Category:
-            </label>
-            <select
-              id="categorySelect"
-              className={`font-teacher w-full rounded border border-black p-2 text-center text-base ${theme.isDarkMode ? "bg-gray-700 text-white" : "bg-white text-black"}`}
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
+          {visibilityStates.isCategorySelectVisible && (
+            <div className="mb-4 flex max-w-[300px] items-center justify-center">
+              <label htmlFor="categorySelect" className="mr-2">
+                Category:
+              </label>
+              <select
+                id="categorySelect"
+                className={`font-teacher w-full rounded border border-black p-2 text-center text-base ${theme.isDarkMode ? "bg-gray-700 text-white" : "bg-white text-black"}`}
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                {categories.map((cat) => (
+                  <option key={cat} value={cat}>
+                    {cat}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+
+          {visibilityStates.isPublicCheckboxVisible && (
+            <div className="mt-2.5 flex items-center text-sm">
+              <input
+                type="checkbox"
+                id="publicCheckbox"
+                checked={isPublic}
+                onChange={() => setIsPublic(!isPublic)}
+                title="Share collection publicly"
+                className={`relative mr-2.5 size-5 cursor-pointer appearance-none rounded border-2 ${theme.isDarkMode ? "border-gray-600 bg-gray-700" : "border-gray-300 bg-white"} checked:bg-blue-500 checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:-translate-x-1/2 checked:after:-translate-y-1/2 checked:after:text-sm checked:after:text-white checked:after:content-["✓"]`}
+              />
+              <label htmlFor="publicCheckbox">
+                I want to share my collection publicly
+              </label>
+            </div>
+          )}
+
+          {visibilityStates.isSubmitButtonVisible && (
+            <button
+              type="button"
+              onClick={handleNext}
+              className="submit-collection-button bg-light-blue hover:bg-hover-blue active:bg-active-blue mt-5 max-w-[300px] cursor-pointer rounded border border-black p-2.5 text-base font-bold uppercase text-black transition-all duration-300 hover:scale-105 active:scale-95"
             >
-              {categories.map((cat) => (
-                <option key={cat} value={cat}>
-                  {cat}
-                </option>
-              ))}
-            </select>
-          </div>
-        )}
+              Next
+            </button>
+          )}
 
-        {visibilityStates.isPublicCheckboxVisible && (
-          <div className="mt-2.5 flex items-center text-sm">
-            <input
-              type="checkbox"
-              id="publicCheckbox"
-              checked={isPublic}
-              onChange={() => setIsPublic(!isPublic)}
-              title="Share collection publicly"
-              className={`relative mr-2.5 size-5 cursor-pointer appearance-none rounded border-2 ${theme.isDarkMode ? "border-gray-600 bg-gray-700" : "border-gray-300 bg-white"} checked:bg-blue-500 checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:-translate-x-1/2 checked:after:-translate-y-1/2 checked:after:text-sm checked:after:text-white checked:after:content-["✓"]`}
-            />
-            <label htmlFor="publicCheckbox">
-              I want to share my collection publicly
-            </label>
-          </div>
-        )}
+          {/* Add the GuidedTour component here */}
+          <GuidedTour
+            steps={steps}
+            isRunning={isTourRunning}
+            onComplete={handleTourComplete} // Use the new handler
+            currentStep={currentTourStep}
+            onStepChange={handleTourStepChange}
+            tourName="newCollection"
+          />
 
-        {visibilityStates.isSubmitButtonVisible && (
           <button
             type="button"
-            onClick={handleNext}
-            className="submit-collection-button bg-light-blue hover:bg-hover-blue active:bg-active-blue mt-5 max-w-[300px] cursor-pointer rounded border border-black p-2.5 text-base font-bold uppercase text-black transition-all duration-300 hover:scale-105 active:scale-95"
+            onClick={() => setShowFeedback(true)}
+            className="mt-4 rounded border border-black bg-blue-500 px-4 py-2 text-white"
           >
-            Next
+            Give Feedback
           </button>
-        )}
 
-        {/* Add the GuidedTour component here */}
-        <GuidedTour
-          steps={steps}
-          isRunning={isTourRunning}
-          onComplete={handleTourComplete} // Use the new handler
-          currentStep={currentTourStep}
-          onStepChange={handleTourStepChange}
-          tourName="newCollection"
-        />
-
-        <button
-          type="button"
-          onClick={() => setShowFeedback(true)}
-          className="mt-4 rounded border border-black bg-blue-500 px-4 py-2 text-white"
-        >
-          Give Feedback
-        </button>
-
-        {showFeedback && (
-          <FeedbackForm onClose={() => setShowFeedback(false)} />
-        )}
+          {showFeedback && (
+            <FeedbackForm onClose={() => setShowFeedback(false)} />
+          )}
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
