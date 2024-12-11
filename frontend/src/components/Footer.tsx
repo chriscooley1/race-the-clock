d07dd79d0c578d276acb5717de9e48907dca2c3d
Chris Cooley <ccooley@bonadmin.com>
2024-12-11 08:34:49 -0700

    rearrange footer and remove extra resource link

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -23,19 +23,13 @@ const Footer: React.FC = () => {
           <div>
             <ul className="space-y-2">
               <li>
-                <a href="/about" className="hover:text-gray-300">Contact us</a>
+                <a href="/about" className="hover:text-gray-300">Contact Us</a>
               </li>
               <li>
-                <a href="/resources" className="hover:text-gray-300">Resources and Tutorials</a>
-              </li>
-              <li>
-                <a href="/resources" className="hover:text-gray-300">FAQ's</a>
+                <a href="/resources" className="hover:text-gray-300">Help Center</a>
               </li>
               <li>
                 <a href="/my-account" className="hover:text-gray-300">Account</a>
-              </li>
-              <li>
-                <a href="/shop" className="hover:text-gray-300">Shop</a>
               </li>
             </ul>
           </div>
@@ -49,6 +43,13 @@ const Footer: React.FC = () => {
               </li>
               <li>
                 <a href="/discover-collections" className="hover:text-gray-300">Discover</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul className="space-y-2">
+              <li>
+                <a href="/shop" className="hover:text-gray-300">Shop</a>
               </li>
               <li>
                 <a href="/games" className="hover:text-gray-300">Games</a>
